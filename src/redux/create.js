c7c3c1a019c8fcafa3c4533a99a9737e0145904a
tonyYo <420156367@qq.com>
2017-10-11 19:05:35 +0800

    删掉了css 的组件

--- src/redux/create.js
+++ src/redux/create.js
@@ -2,7 +2,7 @@ import { createStore as _createStore, applyMiddleware, compose } from 'redux';
 import createMiddleware from './middleware/clientMiddleware';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import Immutable from 'immutable';
+// import Immutable from 'immutable';
 
 export default function createStore(history, client, data) {
   // Sync dispatched route actions to the history
@@ -24,9 +24,9 @@ export default function createStore(history, client, data) {
   }
 
   const reducer = require('./modules/reducer');
-  if (data) {
-    data.pagination = Immutable.fromJS(data.pagination);
-  }
+  // if (data) {
+  //   data.pagination = Immutable.fromJS(data.pagination);
+  // }
   const store = finalCreateStore(reducer, data);
 
 
