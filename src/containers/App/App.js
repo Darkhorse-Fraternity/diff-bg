482693a037e522f06d3ec824f16e248c33bfcdcb
tonyYo <420156367@qq.com>
2017-10-13 19:27:39 +0800

    no message

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { IndexLink } from 'react-router';
-// import { LinkContainer } from 'react-router-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
 import Helmet from 'react-helmet';
 import { logout } from 'redux/modules/auth';
 import { push } from 'react-router-redux';
@@ -59,32 +55,15 @@ export default class App extends Component {
   render() {
     const {user} = this.props;
     // const styles = require('./App.scss');
+    console.log('test:', user);
 
     return (
       <div >
         <Helmet {...config.app.head}/>
-        <Navbar fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
-                <div/>
-                <span>{config.app.title}</span>
-              </IndexLink>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
-
-          <Navbar.Collapse eventKey={0}>
-
-            {user &&
-            <p className={' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
-            <Nav navbar pullRight>
-              <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
-                <i className="fa fa-github"/>
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
+          <div/>
+          <span>{config.app.title}</span>
+        </IndexLink>
 
         <div >
           {this.props.children}
