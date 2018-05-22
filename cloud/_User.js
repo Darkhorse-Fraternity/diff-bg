4d6c7abd84d0ae046293d55b71d02e38dab0bc95
tonyYo <420156367@qq.com>
2018-05-22 17:43:01 +0800

    fix

--- cloud/_User.js
+++ cloud/_User.js
@@ -4,7 +4,7 @@ const AV = require('leanengine');
 //当用户通过邮箱或者短信验证时，对该用户执行特定操作。比如：
 AV.Cloud.onLogin(function(request) {
   // 因为此时用户还没有登录，所以用户信息是保存在 request.object 对象中
-  console.log("on login:", request.object);
+  // console.log("on login:", request.object);
   if (request.object.get('username') === 'noLogin') {
     // 如果是 error 回调，则用户无法登录（收到 401 响应）
     throw new AV.Cloud.Error('Forbidden');
